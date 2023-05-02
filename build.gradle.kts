6efba70e992d6b3bc32eff20619966141fc7a8cd
Marcel Maatkamp <m.maatkamp@gmail.com>
2023-05-02 20:03:05 +0200

    updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.7.8"
+    id("org.springframework.boot") version "2.7.11"
     id("io.spring.dependency-management") version "1.1.0"
 }
 
@@ -31,7 +31,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework:spring-websocket")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("io.projectreactor.tools:blockhound:1.0.7.RELEASE")
+    implementation("io.projectreactor.tools:blockhound:1.0.8.RELEASE")
     implementation("io.projectreactor:reactor-tools")
     implementation("org.clojure:clojure:1.11.1")
     implementation("org.msync:spring-boost:0.2.0")
